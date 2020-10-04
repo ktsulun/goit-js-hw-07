6bda234f92af76807e31f84a8799cda0b780b2f2
ktsulun <ktsulun@gmail.com>
2020-10-05 01:10:04 +0300

    t1 and 2 commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,25 @@
 const categoriesItemsArray = document.querySelectorAll(`.item`);
-console.log(`В списке ` + categoriesItemsArray.length + ` категории`);
+console.log(`В списке ${categoriesItemsArray.length} категории`);
 
-console.log(categoriesItemsArray);
+// console.log(categoriesItemsArray);
 
-for (let i = 0; i < categoriesItemsArray.length; i += 1) {
-  const categoriesItemTitle = categoriesItemsArray[i].querySelector(`h2`);
-  const categoryList = categoriesItemsArray[i].querySelectorAll(`li`);
+// for (let i = 0; i < categoriesItemsArray.length; i += 1) {
+//   const categoriesItemTitle = categoriesItemsArray[i].querySelector(`h2`);
+//   const categoryList = categoriesItemsArray[i].querySelectorAll(`li`);
+
+//   console.log(
+//     `Категория: ` +
+//       categoriesItemTitle.textContent +
+//       `\nКоличество элементов: ` +
+//       categoryList.length
+//   );
+// }
+
+categoriesItemsArray.forEach((element) => {
+  const categoriesItemTitle = element.querySelector(`h2`);
+  const categoryList = element.querySelectorAll(`li`);
 
   console.log(
-    `Категория: ` +
-      categoriesItemTitle.textContent +
-      `\nКоличество элементов: ` +
-      categoryList.length
+    `Категория: ${categoriesItemTitle.textContent} \nКоличество элементов: ${categoryList.length}`
   );
-}
+});
--- js/task-2.js
+++ js/task-2.js
@@ -8,10 +8,14 @@ const ingredients = [
 ];
 const ingredientsListEl = document.querySelector(`#ingredients`);
 let ingredientsListItemArray = [];
-for (let i = 0; i < ingredients.length; i += 1) {
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const ingredientsListItem = document.createElement("li");
+//   ingredientsListItem.textContent = ingredients[i];
+//   ingredientsListItemArray.push(ingredientsListItem);
+// }
+ingredients.forEach((element) => {
   const ingredientsListItem = document.createElement("li");
-  ingredientsListItem.textContent = ingredients[i];
+  ingredientsListItem.textContent = element;
   ingredientsListItemArray.push(ingredientsListItem);
-}
-
+});
 ingredientsListEl.append(...ingredientsListItemArray);
